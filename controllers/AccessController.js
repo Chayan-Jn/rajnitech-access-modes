92180c53d3fb27dbc88189bcce69340a5ca75188
chayan <chayanj39@gmail.com>
2025-09-03 23:32:17 +0530

    created the outline of the the handleMockTEst function. This function handles the mock tests and uses test engine controller to start the test

--- controllers/AccessController.js
+++ controllers/AccessController.js
@@ -1,40 +1,57 @@
 
-
 async function handleAccessMode(req, res) {
-
     try {
-        const test = await findTestById(req.params.id);  //assuming this function gets the test from the mysql database
+        const test = await findTestById(req.params.id);  //** assuming this function gets the test from the mysql database
         if(!test){
             return res.status(404).json({
                 success:false,
                 message:"Test not found "
             })
         }
-        const test_type = test.type; // mock/live
-        const user = req.user;
-        const user_institute_id = req.user?.institute_id;
-        const access_mode = req.access_mode;
-
+        const test_type = test.type; // mock / live
+        if(test_type === "mock"){
+            return handleMockTest(req,res,test);
+        }
     }
     catch (err) {
         console.log(err);
+        res.status(500).json({
+            success:false,
+            message:"Error occured while handling access mode"
+        })
     }
 }
-/* ????? How to get the time according to the timezone of the user1
- date = new Date()
- const istDate = date.toLocaleString("en-US", {
-     timeZone: "Asia/Kolkata"
- });
- console.log(istDate)
 
- How to find out the correct time of the user ???
- 1. Get the time zone of the user from the frontend
- 2. Use luxon to find the correct time  
-    const { DateTime } = require("luxon");
-    const istTime = DateTime.fromISO("2025-09-02T17:47:19.010Z", { zone: "utc" })
-    .setZone("Asia/Kolkata");
-    console.log(istTime.toFormat("yyyy-MM-dd HH:mm:ss")); 
-    // Output: "2025-09-02 23:17:19"
-*/
+async function handleMockTest(req,res,test) {
+    try{
+        const user = req.user;
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        const user_attempts = await getUserAttempts(test.id,user.id); //** 
+        const max_attemps = getMaxAttempts(test) || 5; //** 
+        if(user_attempts >= max_attemps){
+            return res.status(400).json({
+                success:false,
+                message:"You have reached the max number of attempts for this test"
+            })
+        }
+        return testEngineController.startTest(req,res,user,test); //** 
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({
+            success:false,
+            message:"Error occured while processing the mock test"
+        })
+    }
+}
 
-  
+//** -> create/add the function after getting the controllers/functions from others
+
+// const user = req.user;
+// const user_institute_id = req.user?.institute_id;
+// const access_mode = req.access_mode;
